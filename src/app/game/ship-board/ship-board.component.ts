95e56effabb93dc2d2859872e732495678355382
Jimmy Acero <j.acero@orekaconsultores.com>
2020-03-06 14:39:20 -0500

    Eliminando código inútil de ShipBoard

--- src/app/game/ship-board/ship-board.component.ts
+++ src/app/game/ship-board/ship-board.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Cell } from '../models/cell';
 import { CELLS, CELLSIZE } from '../constants';
 import { indexToCell } from '../functions';
-import { CdkDrag } from '@angular/cdk/drag-drop';
 import { Ship } from '../models/ship';
 import { ShipFactory } from '../models/ship-factory';
 
@@ -27,9 +26,6 @@ export class ShipBoardComponent {
 		ShipFactory.create('1', {left: CELLSIZE * 9, top: 0}, 0),
 	]
 
-	r = 0;
-	lastDragging = false;
-
 	constructor() {
 		this.cells = Array<Cell>(CELLS * CELLS).fill({
 			column: 0,
@@ -37,33 +33,4 @@ export class ShipBoardComponent {
 			state: 'inactive'
 		}).map<Cell>((c, i)=>indexToCell(i));
 	}
-	// nearCell(ship:CdkDrag){
-	// 	this.lastDragging = true;
-	// 	const pos = ship._dragRef.getFreeDragPosition();
-	// 	const [x,y] = [Math.round(+pos.x / CELLSIZE) * CELLSIZE, Math.round(+pos.y / CELLSIZE) * CELLSIZE];
-	// 	ship.element.nativeElement.classList.add('animate');
-	// 	ship._dragRef.setFreeDragPosition({x, y});
-	// 	setTimeout(() => {
-	// 		ship.element.nativeElement.classList.remove('animate');
-	// 	}, 205);
-	// }
-	// rot(ship:CdkDrag){
-	// 	if(!this.lastDragging){
-	// 		this.r = (this.r + 90) % 360;
-	// 		const pos = ship._dragRef.getFreeDragPosition();
-	// 		if(this.r == 0){
-	// 			const [x,y] = [(Math.round(+pos.x / CELLSIZE) * CELLSIZE) + CELLSIZE, (Math.round(+pos.y / CELLSIZE) * CELLSIZE) - CELLSIZE];
-	// 			ship._dragRef.setFreeDragPosition({x, y});
-	// 		}
-	// 		if(this.r == 90){
-	// 			const [x,y] = [Math.round(+pos.x / CELLSIZE) * CELLSIZE, (Math.round(+pos.y / CELLSIZE) * CELLSIZE) + CELLSIZE];
-	// 			ship._dragRef.setFreeDragPosition({x, y});
-	// 		}
-	// 		if(this.r == 270){
-	// 			const [x,y] = [(Math.round(+pos.x / CELLSIZE) * CELLSIZE) - CELLSIZE, (Math.round(+pos.y / CELLSIZE) * CELLSIZE)];
-	// 			ship._dragRef.setFreeDragPosition({x, y});
-	// 		}
-	// 	}
-	// 	this.lastDragging = false;
-	// }
 }
